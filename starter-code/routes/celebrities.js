908d47cd2fb914b4e12250e15aca038f6d3d87c2
Dhiren Brahmbhatt <dhiren.b94@gmail.com>
2021-04-20 17:02:44 +0100

    iterations 0-6 (celebrities) done

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -10,13 +10,47 @@ router.get('/celebrities', async (req, res, next) => {
       res.render('celebrities/list', {celebritiesFromDB})
   });
 
-router.get('/celebrities/:id', async (req, res) => {
-    const celebritiesId = req.params.id;
-    const celebrities = await CelebrityModel.findById(celebritiesId);
-    res.render('celebrities/details', {celebrities});
+
+  
+router.get('/celebrities/add', async (req, res, next) => {
+      res.render('celebrities/new')
+    });
+    
+router.post('/celebrities/add', async (req, res, next) => {
+      const {name, occupation, catchPhrase} = req.body
+      await CelebrityModel.create(req.body)
+      res.redirect('/celebrities')
 })
 
 
 
+router.get('/celebrities/:id', async (req, res) => {
+        const celebritiesId = req.params.id;
+        const celebrities = await CelebrityModel.findById(celebritiesId);
+        res.render('celebrities/details', {celebrities});
+    });
 
-  module.exports = router;
+
+router.get('/celebrities/:id/edit', async (req, res, next) => {
+        const celebId = req.params.id;
+        const celeb = await CelebrityModel.findById(celebId)
+        res.render('celebrities/edit', {celeb})
+});
+      
+router.post('/celebrities/:id/edit', async (req, res, next) => {
+        const celebId = req.params.id
+        const {name, occupation, catchPhrase} = req.body
+        await CelebrityModel.findByIdAndUpdate(celebId, req.body);
+        res.redirect('/celebrities')
+});
+
+
+
+router.post('/celebrities/:id/delete', async (req, res, next) => {
+        const celebId = req.params.id
+        await CelebrityModel.findByIdAndDelete(celebId)
+        res.redirect('/celebrities')
+});
+
+
+module.exports = router;
